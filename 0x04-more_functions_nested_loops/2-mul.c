5f3721a870a17c043f7adbe26d81eeefa56f45c2
Iris134340 <el.chaimaa@gmail.com>
2023-03-24 19:25:37 -0700

    pultiply a by b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of 2 integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: mul of a by b results
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
