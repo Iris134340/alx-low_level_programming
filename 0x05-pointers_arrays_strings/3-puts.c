3278fc5a8ee2555d83223fac7de83b59e4707eb1
Iris134340 <el.chaimaa@gmail.com>
2023-03-27 13:51:34 -0700

    print a string of characters with a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string of characters
+ *
+ * @str: the string of characters
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	_putchar('\n');
+}
