d9f25fb734562a93576b1f637ec10ba14236b1a7
Iris134340 <el.chaimaa@gmail.com>
2023-03-27 15:32:50 -0700

    print strings in rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string of characters in reverse
+ *
+ * @s: the string of characters
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int size_arr = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		size_arr++;
+	}
+
+	for (i = size_arr; s[i] >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
