63fc0d8b42a38684fd995be8b4dcd347986985c6
Iris134340 <el.chaimaa@gmail.com>
2023-03-27 09:36:34 -0700

    betty style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,9 @@
 #include "main.h"
 
-/** rset_to_98 - resets the value of the variable n to 98
+/**
+ *  reset_to_98 - resets the value of the variable n to 98
+ *
+ *  @n: pointer
  *
  * Return: 0
  */
