8b0510cd4a8feae4b2db40b09d5b8872ca5b8109
Iris134340 <el.chaimaa@gmail.com>
2023-03-28 11:43:18 -0700

    attempt

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,10 +13,10 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0 ; a[i] < a[n] ; i++)
+	for (i = 0 ; i < n ; i++)
 	{
 		printf("%d", a[i]);
-		if (i < n - 1)
+		if (i != 0)
 		{
 			printf(", ");
 		}
