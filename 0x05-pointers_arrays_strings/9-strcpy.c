72cd26352c1c9cdbfe7191ae3d19b81941fb364a
Iris134340 <el.chaimaa@gmail.com>
2023-03-28 19:02:55 -0700

    copies a string to another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string to another
+ *
+ * @src: the array we'll copy from
+ * @dest: the array we'll copy to
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
