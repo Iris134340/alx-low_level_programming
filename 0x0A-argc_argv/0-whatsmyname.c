71d88d03497747c8c9898c746ea1fd9f79d01e69
Iris134340 <el.chaimaa@gmail.com>
2023-04-15 18:38:22 -0700

    my name is program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints program name
+ *
+ * @argc: argument 1
+ * @argv: argument 2
+ *
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
